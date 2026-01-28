d92a0f8787e53d47f6f8444d09e8076b4a7bb976
David Su <david.d.su@gmail.com>
2026-01-28 02:16:03 -0800

    Implement audio

---
+++ Assets/Scripts/CollisionAudioEmitter.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+/** Emitter that plays audio when collided with **/
+
+[RequireComponent(typeof(AudioSource))]
+public class CollisionAudioEmitter : MonoBehaviour
+{
+    [SerializeField]
+    float maxCollisionVelocity = 15f; // Max velocity to use for collisions
+
+    AudioSource audioSource; // Audio source used for playing SFX
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Set the audio source
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Called when another object collides with this game object
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Ouch!");
+
+        PlayAudio(collision.relativeVelocity.magnitude);
+    }
+
+    // Play audio for collision
+    void PlayAudio(float velocity)
+    {
+        if (audioSource != null)
+        {
+            // Scale audio source's volume based on collision velocity...
+            audioSource.volume = Mathf.Clamp01(velocity / maxCollisionVelocity);
+
+            // ... then play the audio source
+            audioSource.Play();
+
+            Debug.LogFormat("Collision: Velocity = {0}, Volume = {1}", velocity, audioSource.volume);
+        }
+    }
+}
