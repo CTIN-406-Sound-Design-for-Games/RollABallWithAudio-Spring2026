537dcd500dd4f123cb8888869bda88b628ca25f9
David Su <david.d.su@gmail.com>
2026-02-04 00:30:45 -0800

    Implement middleware example (converting existing audio impl from Unity to Wwise)

--- Assets/Scripts/CollisionAudioEmitter.cs
+++ Assets/Scripts/CollisionAudioEmitter.cs
@@ -1,21 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+
 using UnityEngine;
 
 /** Emitter that plays audio when collided with **/
 
-[RequireComponent(typeof(AudioSource))]
 public class CollisionAudioEmitter : MonoBehaviour
 {
     [SerializeField]
+    AK.Wwise.Event collisionEvent;
+
+    [SerializeField]
+    AK.Wwise.RTPC collisionVelocityNormalizedRtpc;
+
+    [SerializeField]
     float maxCollisionVelocity = 15f; // Max velocity to use for collisions
-
-    AudioSource audioSource; // Audio source used for playing SFX
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        // Set the audio source
-        audioSource = GetComponent<AudioSource>();
-    }
 
     // Called when another object collides with this game object
     void OnCollisionEnter(Collision collision)
@@ -28,15 +27,14 @@ public class CollisionAudioEmitter : MonoBehaviour
     // Play audio for collision
     void PlayAudio(float velocity)
     {
-        if (audioSource != null)
-        {
-            // Scale audio source's volume based on collision velocity...
-            audioSource.volume = Mathf.Clamp01(velocity / maxCollisionVelocity);
+        // Scale the volume based on collision velocity...
+        // (we can still do the normalizing on the game-engine-side if we'd like)
+        float velocityNormalized = Mathf.Clamp01(velocity / maxCollisionVelocity);
+        collisionVelocityNormalizedRtpc.SetValue(this.gameObject, velocityNormalized);
 
-            // ... then play the audio source
-            audioSource.Play();
+        // ... then post the event
+        collisionEvent.Post(this.gameObject);
 
-            Debug.LogFormat("Collision: Velocity = {0}, Volume = {1}", velocity, audioSource.volume);
-        }
+        Debug.LogFormat("Collision: Velocity={0}, Normalized={1}", velocity, velocityNormalized);
     }
 }
---
+++ RollABallWithAudio-Spring2026_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,46 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_AMB_CITY_LP = 584628495U;
+        static const AkUniqueID PLAY_AMB_FOREST_LP = 149137005U;
+        static const AkUniqueID PLAY_AMB_INTERIOR_LP = 93749488U;
+        static const AkUniqueID PLAY_IMP_CRATE = 38705144U;
+        static const AkUniqueID PLAY_IMP_WALL = 2242885151U;
+        static const AkUniqueID PLAY_MVT_BALLROLLING_LP = 4193143717U;
+        static const AkUniqueID PLAY_UI_WIN = 2823495579U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID RTPC_BALLSPEED = 3709966389U;
+        static const AkUniqueID RTPC_COLLISIONVELOCITYNORMALIZED = 1573010119U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AMB = 1117531639U;
+        static const AkUniqueID MAIN_AUDIO_BUS = 2246998526U;
+        static const AkUniqueID SFX = 393239870U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
